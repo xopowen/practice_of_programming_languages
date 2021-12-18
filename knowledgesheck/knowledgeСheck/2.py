c5225582229d4cdc4687249338836246b2fca827
Женя Хороших <xopowen@gmail.com>
2021-12-18 12:20:15 +0800

    task hashMap  Ok

--- knowledgesheck/knowledgeСheck/2.py
+++ knowledgesheck/knowledgeСheck/2.py
@@ -1,9 +1,11 @@
 import re
 
 lett = "AIEOUY"
-word = input()
+lett2 = "QWRTYPSDFGHJKLZXCVNM"
+word = input('тут нужно что то...слово, фраза, мат:')
 n = sum((1 for i in word.upper() if i in lett))
+m = sum((1 for i in word.upper() if i in lett2))
 if n != None:
-    print(n)
+    print("гласных: " + str(n),"согласные: " + str(m))
 else:
     print("0")
